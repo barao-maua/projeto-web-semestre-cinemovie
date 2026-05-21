64be6c63d6dfef4ced6297e1ed1dba6805a4f947
schups-gt <gabrielsberaldo2015@gmail.com>
2026-05-21 19:52:02 -0300

    commit final E3 perdão newton enviei errado na ultima

--- start-frontend.ps1
+++
@@ -1,5 +0,0 @@
-$projectRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location "$projectRoot\frontend"
-
-Write-Host "Subindo frontend Vite em http://localhost:5173 ..." -ForegroundColor Green
-npm run dev
